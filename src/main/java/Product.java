cbe020e15db42562aab52c705b012e7512983290
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-05 11:42:52 +0100

    pipeline

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,8 +1,10 @@
 import lombok.Value;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -16,23 +18,33 @@ class Product {
 @Value
 class Packed {
     Product product;
+
+    static CompletableFuture<Packed> pack(Product product) {
+        return CompletableFuture.supplyAsync(() -> new Packed(product));
+    }
 }
 
 @Value
 class Send {
     Packed packed;
+    
+    static CompletableFuture<Send> send(Packed packed) {
+        return CompletableFuture.supplyAsync(() -> new Send(packed));
+    }
 }
 
 class XXX {
     public static void main(String[] args) {
-        var collected = Stream.of(1, 2, 3, 4)
+        var futures = Stream.of(1, 2, 3, 4)
                 .map(id -> CompletableFuture.supplyAsync(() -> new Product(id)))
-                .map(product -> product.thenCompose(p -> CompletableFuture.supplyAsync(() -> new Packed(p))))
-                .map(packed -> packed.thenCompose(p -> CompletableFuture.supplyAsync(() -> new Send(p))))
+                .map(product -> product.thenCompose(Packed::pack))
+                .map(packed -> packed.thenCompose(Send::send))
                 .map(send -> send.thenApply(Send::toString))
+                .map(future -> future.orTimeout(500, TimeUnit.MILLISECONDS))
+                .map(future -> future.handle((ok, ex) -> nonNull(ok) ? ok : "FAILED: " + ex))
                 .collect(toList());
 
-        collected.stream()
+        futures.stream()
                 .map(CompletableFuture::join)
                 .forEach(System.out::println);
     }
