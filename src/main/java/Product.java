a900904d00b29bf4fda6f2099974119b8c16a4b1
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-05 17:10:22 +0100

    clean code refactoring: returning function + meaningful functions

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -2,6 +2,7 @@ import lombok.Value;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Function;
 
 /**
  * Created by mtumilowicz on 2019-01-04.
@@ -15,8 +16,8 @@ class Product {
 class Packed {
     Product product;
 
-    static CompletableFuture<Packed> pack(Product product, Executor executor) {
-        return CompletableFuture.supplyAsync(() -> new Packed(product), executor);
+    static Function<Product, CompletableFuture<Packed>> pack(Executor executor) {
+        return product -> CompletableFuture.supplyAsync(() -> new Packed(product), executor);
     }
 }
 
@@ -24,7 +25,7 @@ class Packed {
 class Send {
     Packed packed;
     
-    static CompletableFuture<Send> send(Packed packed, Executor executor) {
-        return CompletableFuture.supplyAsync(() -> new Send(packed), executor);
+    static Function<Packed, CompletableFuture<Send>> send(Executor executor) {
+        return packed -> CompletableFuture.supplyAsync(() -> new Send(packed), executor);
     }
 }
--- src/main/java/ProductService.java
+++ src/main/java/ProductService.java
@@ -9,17 +9,17 @@ import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toList;
 
 /**
- * Created by mtumilowicz on 2019-01-05.
+ * Created by mtumilowicz by 2019-01-05.
  */
 class ProductService {
     List<String> pack(List<Integer> ids) {
 
-        var executor = productThreadPool(ids.size());
+        var executors = productThreadPool(ids.size());
 
         var sendFutures = IntStream.range(1, 300)
-                .mapToObj(id -> CompletableFuture.supplyAsync(() -> new Product(id), executor))
-                .map(product -> product.thenCompose(p -> Packed.pack(p, executor)))
-                .map(packed -> packed.thenCompose(p -> Send.send(p, executor)))
+                .mapToObj(id -> CompletableFuture.supplyAsync(() -> new Product(id), executors))
+                .map(product -> product.thenCompose(Packed.pack(by(executors))))
+                .map(packed -> packed.thenCompose(Send.send(by(executors))))
                 .map(send -> send.thenApply(Send::toString))
                 .map(future -> future.orTimeout(500, TimeUnit.MILLISECONDS))
                 .map(future -> future.handle((ok, ex) -> nonNull(ok) ? ok : "FAILED: " + ex))
@@ -37,5 +37,9 @@ class ProductService {
                     t.setDaemon(true);
                     return t;
                 });
+    }
+
+    private Executor by(Executor executor) {
+        return executor;
     }
 }
